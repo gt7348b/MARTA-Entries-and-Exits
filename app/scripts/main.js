0faed001c396b31086706f982b3acc59d2369c3a
John Crocker <johnniecrocker@yahoo.com>
2015-01-31 17:07:29 -0500

    cleaned data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+console.log('Roscoe is a cutie');
+
+//Call Data
+
+var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
+      console.log(data);
+
+      //Cleans data to process
+      var exits = data.map(function(t, exit){
+
+        var exit = d3.keys(data[0])
+          .filter(function(key){ return key!=='Name'})
+          .filter(function(key){ return key!=='WHERE GET ON'})
+          .filter(function(key){ return key!==''});
+
+        return {
+          name: t.Name,
+          exitst: exit.map(function(d){
+            return {station: d, ex: t[d]}
+          })
+        }
+      });
+
+
+});
