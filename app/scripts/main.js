f44389e83879a510c55d95dc540120503a5903e0
John Crocker <johnniecrocker@yahoo.com>
2015-01-31 18:06:05 -0500

    working on selecting station

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,6 +10,31 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
         station = $('#station').val();
 
         console.log(station);
+
+        var response = [];
+
+        sel_station = data.filter(function(entry){
+          if (entry.Name == station){
+            response.push(entry);
+          }
+        });
+
+        console.log(response);
+
+        var exits = response.map(function(t, exit){
+
+          var exit = d3.keys(data[0])
+          .filter(function(key){ return key!=='Name'})
+          .filter(function(key){ return key!=='WHERE GET ON'})
+          .filter(function(key){ return key!==''});
+
+          return {
+            exitst: exit.map(function(d){
+              return {station: d, ex: t[d]}
+            })
+          }
+        });
+        console.log(exits);
 });
 
       //Cleans data to process
@@ -43,5 +68,5 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
         .append('text')
         .text(function (d){d.exitst[17].ex});
 
-    
+
 });
