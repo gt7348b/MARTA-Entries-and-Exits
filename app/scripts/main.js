a5f9edc306926dd57be117b9984489e3cc6793cf
John Crocker <johnniecrocker@yahoo.com>
2015-02-01 14:40:55 -0500

    using submit button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,7 @@ console.log('Roscoe is a cutie');
 var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
       console.log(data);
 
-      $('#station').on('click', function(event){
+      $('#submit').on('click', function(event){
         event.preventDefault();
         station = $('#station').val();
 
@@ -35,25 +35,25 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
           }
         });
         console.log(exits);
-});
 
-      //Cleans data to process
-      var exits = data.map(function(t, exit){
 
-        var exit = d3.keys(data[0])
-          .filter(function(key){ return key!=='Name'})
-          .filter(function(key){ return key!=='WHERE GET ON'})
-          .filter(function(key){ return key!==''});
-
-        return {
-          name: t.Name,
-          exitst: exit.map(function(d){
-            return {station: d, ex: t[d]}
-          })
-        }
-      });
-
-      console.log(exits);
+      // //Cleans data to process
+      // var exits = data.map(function(t, exit){
+      //
+      //   var exit = d3.keys(data[0])
+      //     .filter(function(key){ return key!=='Name'})
+      //     .filter(function(key){ return key!=='WHERE GET ON'})
+      //     .filter(function(key){ return key!==''});
+      //
+      //   return {
+      //     name: t.Name,
+      //     exitst: exit.map(function(d){
+      //       return {station: d, ex: t[d]}
+      //     })
+      //   }
+      // });
+      //
+      // console.log(exits);
 
       //render the bar chart
       d3.select('#div1').selectAll('p')
@@ -62,11 +62,11 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
         .append('div')
         .attr('class', 'bar')
         .style('height', function (d){
-          var barHeight = d.exitst[0].ex /3;
+          var barHeight = d.exitst[9].ex /3;
           return barHeight + 'px';
         })
         .append('text')
-        .text(function (d){d.exitst[17].ex});
-
+        .text(function (d){d.exitst[0].ex});
+      });
 
 });
