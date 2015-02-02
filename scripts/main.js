9bd69b7471b56432d733dd5ed87ae59f70dde7f8
John Crocker <johnniecrocker@yahoo.com>
2015-02-02 12:22:43 -0500

    gulped

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,8 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
 
         console.log(station);
 
+        d3.select('svg').remove()
+
         var response = [];
 
         sel_station = data.filter(function(entry){
@@ -36,19 +38,60 @@ var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
           //return exit;
 
           //render the bar chart
-          d3.select('#div1').selectAll('p')
-          .data(exit)
-          .enter()
-          .append('div')
-          .attr('class', 'bar')
-          .style('height', function (d){
-            var barHeight = d.ex /3;
-            return barHeight + 'px';
-          })
-          .append('text')
-          .text(function (d){d.ex});
+        svg = d3.select('#div1').append('svg')
 
-        });
+        w = 1000;
+        h = 500;
+        barPadding = 1;
+
+        svg.selectAll('rect')
+              .data(exit)
+              .enter()
+              .append('rect')
+              .attr('x', function(d, i){
+                return i * (w/exit.length);
+              })
+              .attr('y', function(d){
+                return h - (d.ex/2);
+              })
+              .attr('width', w / exit.length - barPadding)
+              .attr('height', function(d){
+                return d.ex/2;
+              })
+              .attr('fill', 'teal');
+
+            svg.selectAll('text')
+              .data(exit)
+              .enter()
+              .append('text')
+              .text(function(d){
+                return d.station;
+              })
+              .attr('x', function(d, i){
+                return i * (w/exit.length) + 10;
+              })
+              .attr('y', function(d){
+                return h - (d.ex/2) + 10;
+              })
+              .attr('font-family', 'sans-serif')
+              .attr('font-size', '11px')
+              .attr('fill', 'white')
+              .attr('text-anchor', 'middle');
+
+
+        //   d3.select('#div1').selectAll('p')
+        //   .data(exit)
+        //   .enter()
+        //   .append('div')
+        //   .attr('class', 'bar')
+        //   .style('height', function (d){
+        //     var barHeight = d.ex /3;
+        //     return barHeight + 'px';
+        //   })
+        //   .append('text')
+        //   .text(function (d){d.ex});
+        //
+       });
         console.log(exits);
 
 
