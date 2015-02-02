772c5224e6b2e6c31f86d2438a928f3e8d26a734
John Crocker <johnniecrocker@yahoo.com>
2015-02-02 10:20:14 -0500

    gulped

--- scripts/main.js
+++ scripts/main.js
@@ -5,43 +5,83 @@ console.log('Roscoe is a cutie');
 var data = d3.csv('Total_Rail_Entry_Exit_Cleaned.csv', function(error, data){
       console.log(data);
 
-      $('#station').on('click', function(event){
+      $('#submit').on('click', function(event){
         event.preventDefault();
         station = $('#station').val();
 
         console.log(station);
-});
 
-      //Cleans data to process
-      var exits = data.map(function(t, exit){
+        var response = [];
 
-        var exit = d3.keys(data[0])
+        sel_station = data.filter(function(entry){
+          if (entry.Name == station){
+            response.push(entry);
+          }
+        });
+
+        console.log(response);
+
+        var exits = response.map(function(t, exit){
+
+          var sta = d3.keys(data[0])
           .filter(function(key){ return key!=='Name'})
           .filter(function(key){ return key!=='WHERE GET ON'})
           .filter(function(key){ return key!==''});
 
-        return {
-          name: t.Name,
-          exitst: exit.map(function(d){
-            return {station: d, ex: t[d]}
+          var exit = sta.map(function(d){
+              return {station: d, ex: t[d]}
+            });
+          console.log(exit);
+
+          //return exit;
+
+          //render the bar chart
+          d3.select('#div1').selectAll('p')
+          .data(exit)
+          .enter()
+          .append('div')
+          .attr('class', 'bar')
+          .style('height', function (d){
+            var barHeight = d.ex /3;
+            return barHeight + 'px';
           })
-        }
+          .append('text')
+          .text(function (d){d.ex});
+
+        });
+        console.log(exits);
+
+
+      // //Cleans data to process
+      // var exits = data.map(function(t, exit){
+      //
+      //   var exit = d3.keys(data[0])
+      //     .filter(function(key){ return key!=='Name'})
+      //     .filter(function(key){ return key!=='WHERE GET ON'})
+      //     .filter(function(key){ return key!==''});
+      //
+      //   return {
+      //     name: t.Name,
+      //     exitst: exit.map(function(d){
+      //       return {station: d, ex: t[d]}
+      //     })
+      //   }
+      // });
+      //
+      // console.log(exits);
+
+      // //render the bar chart
+      // d3.select('#div1').selectAll('p')
+      //   .data(exits)
+      //   .enter()
+      //   .append('div')
+      //   .attr('class', 'bar')
+      //   .style('height', function (d){
+      //     var barHeight = d.ex /3;
+      //     return barHeight + 'px';
+      //   })
+      //   .append('text')
+      //   .text(function (d){d.ex});
       });
 
-      console.log(exits);
-
-      //render the bar chart
-      d3.select('#div1').selectAll('p')
-        .data(exits)
-        .enter()
-        .append('div')
-        .attr('class', 'bar')
-        .style('height', function (d){
-          var barHeight = d.exitst[0].ex /3;
-          return barHeight + 'px';
-        })
-        .append('text')
-        .text(function (d){d.exitst[17].ex});
-
-    
 });
